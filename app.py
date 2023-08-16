3cac8cfaf03b6c7591394199aa4ae48cd897886a
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-08-16 14:02:43 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FieldList, FormField
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "Very hard to guess string"
+
+class B(FlaskForm):
+    b1 = StringField("B1 Label: ")
+    b2 = StringField("B2 Label: ")
+
+    
+class A(FlaskForm):
+    a1 = StringField("A1 Label: ")
+    a2 = FieldList(FormField(B), min_entries=3)
+    s = SubmitField("Submit")
+
+
+@app.route("/")
+def index():
+    form = A()
+    return render_template("index.html", form=form)
+
+@app.route("/result", methods=["POST"])
+def result():
+    return render_template("result.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
